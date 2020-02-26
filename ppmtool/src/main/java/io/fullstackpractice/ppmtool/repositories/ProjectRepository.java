d8d5963da9963a8f251ea9c0077db583393f3f12
samurisam97 <56446478+samurisam97@users.noreply.github.com>
2020-02-26 14:45:38 -0600

    h2 and repositroy setup

---
+++ ppmtool/src/main/java/io/fullstackpractice/ppmtool/repositories/ProjectRepository.java
@@ -0,0 +1,12 @@
+package io.fullstackpractice.ppmtool.repositories;
+
+import io.fullstackpractice.ppmtool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+    @Override
+    public Iterable<Project> findAllById(Iterable<Long> iterable);
+}
