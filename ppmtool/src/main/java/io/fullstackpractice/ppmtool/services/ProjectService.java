26e83ecb9f4b7c2f18f1d06701578b77b5ee2e3a
samurisam97 <56446478+samurisam97@users.noreply.github.com>
2020-02-27 09:25:20 -0600

    Delete project functionality

--- ppmtool/src/main/java/io/fullstackpractice/ppmtool/services/ProjectService.java
+++ ppmtool/src/main/java/io/fullstackpractice/ppmtool/services/ProjectService.java
@@ -37,4 +37,14 @@ public class ProjectService {
     public Iterable<Project> findAllProjects() {
         return projectRepository.findAll();
     }
+
+    public void deleteProjectByIdentifier(String projectId) {
+        Project project = projectRepository.findByProjectIdentifier(projectId);
+
+        if(project == null){
+            throw new ProjectIdException(("Cannot delete project with ID '"+projectId.toUpperCase()+"'. This project does not exist"));
+        }
+
+        projectRepository.delete(project);
+    }
 }
