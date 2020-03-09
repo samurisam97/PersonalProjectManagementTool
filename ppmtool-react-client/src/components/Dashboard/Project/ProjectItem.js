da310fe837ada13a9f44940ff7eb8f673b955e98
samurisam97 <56446478+samurisam97@users.noreply.github.com>
2020-03-09 12:59:55 -0500

    fixed the project layout so that they appear next to eachother and fill up the screen leaving no wasted space

--- ppmtool-react-client/src/components/Dashboard/Project/ProjectItem.js
+++ ppmtool-react-client/src/components/Dashboard/Project/ProjectItem.js
@@ -9,25 +9,23 @@ const ProjectItem = props => {
       {props.projectsList.map((value, index) => {
         if (!value.deleted) {
           return (
-            <div>
-              <div className={classes.ProjectItem}>
-                <ProjectInstance
-                  key={value.index}
-                  index={value.index}
-                  identification={value.id}
-                  validID={value.validID}
-                  projectTitle={value.title}
-                  validTitle={value.validTitle}
-                  description={value.description}
-                  validDescription={value.validDescription}
-                  updateMode={value.updateMode}
-                  updateText={value.updateText}
-                />
-              </div>
+            <div className={classes.ProjectItem}>
+              <ProjectInstance
+                key={value.index}
+                index={value.index}
+                identification={value.id}
+                validID={value.validID}
+                projectTitle={value.title}
+                validTitle={value.validTitle}
+                description={value.description}
+                validDescription={value.validDescription}
+                updateMode={value.updateMode}
+                updateText={value.updateText}
+              />
             </div>
           );
         } else {
-          return <div className={classes.ProjectItem.deleted}></div>;
+          return null;
         }
       })}
     </div>
